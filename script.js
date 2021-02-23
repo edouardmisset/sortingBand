6332b21985fc643c073f60fb0a886a97503b7632
Edouard Misset <edouardmisset@gmail.com>
2021-02-23 09:34:08 +0100

    first tries

--- script.js
+++ script.js
@@ -2,6 +2,24 @@
 // Variables
 //----------
 
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog',
+];
+
+const list = document.querySelector('ul');
+
 //----------------
 // Event Listeners
 //----------------
@@ -9,3 +27,13 @@
 //----------
 // Functions
 //----------
+
+const sortedBands = [...bands].sort();
+
+list.innerHTML = sortedBands
+  .map((band) => {
+    return `
+        <li>${band}</li>
+      `;
+  })
+  .join('');
